27a04ce69ba712cffa1ea6466e386a28b40546d7
Lena-Krupenko228 <lcrupenko@yandex.ru>
2025-09-21 20:41:20 +0300

    Add files via upload

---
+++ iiuouggoug.cpp
@@ -0,0 +1,20 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    setlocale(LC_ALL, "rus");
+    int centimeters;
+    cout << "Введите длину в сантиметрах: ";
+    cin >> centimeters;
+    int meters = centimeters / 100;
+    int remaining_centimeters = centimeters % 100;
+    cout << "Полных метров: " << meters << endl;
+    cout << "Оставшиеся сантиметры: " << remaining_centimeters << endl;
+
+    return 0;
+
+}
